a6cf3b23b8e2f6d40eaa93a3d66ccd061471500d
Fanter <nightmarelandscape@gmail.com>
2021-06-15 20:13:49 +0300

    client updated

--- Client/client.c
+++ Client/client.c
@@ -4,8 +4,6 @@
 
 int main(int argc,char** argv)
 {
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    printStrAtConsolePos(console, 1, 1, "t", FOREGROUND_RED);
-    printStrAtConsolePos(console, 15, 1, "t2", BACKGROUND_GREEN);
+    startGame(makeWindow(), GetConsoleWindow());
     return 0;
 }
