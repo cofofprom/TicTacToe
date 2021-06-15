13967c05f15e1d95583f6b071a685961a22cca67
Ailurus-code <76646255+Ailurus-code@users.noreply.github.com>
2021-06-15 11:51:38 +0300

    startGame() function fixed, coordinates of each cell found

--- SetField/SetField.c
+++ SetField/SetField.c
@@ -1,24 +1,28 @@
 #include "SetField.h"
 
+
+//Меняет размер окна на 120x30
+//Ставит заголовок
+//Возвращает дексриптор входного потока
 HWND makeWindow()
 {
     HWND stdInput, stdOutput, WindowHandle;
     char Title[] = "TicTacToe";
     COORD bufferSize = {120, 30};
-    SetConsoleTitle(Title);                                                     // Устанваливаем заголовок окна консоли
+    SetConsoleTitle(Title);                                                         // Устанваливаем заголовок окна консоли
     stdInput = GetStdHandle(STD_OUTPUT_HANDLE);                                     // Находим дескриптор окна
     SetConsoleScreenBufferSize(stdOutput, bufferSize);
     WindowHandle = FindWindowA(NULL, Title);
     MoveWindow(WindowHandle, WINDOW_SHIFT_X, WINDOW_SHIFT_Y, WINDOW_WIDTH, WINDOW_HEIGHT, TRUE);//Устанавливаем размеры и смещение окна
-
     return stdInput;                                                                //Возвращаем дескриптор окна
 }
 
-int findWidthOfWindow()                                                         //Найдём ширину окна (сколько символов помещается по горизонтали)
+//Находит ширину окна (в количестве символов)
+int findWidthOfWindow()
 {
-    HWND hWndConsole;                                                           //Дескриптор окна
+    HWND hWndConsole;                                                               //Дескриптор окна
     int Width = 0;
-    if (hWndConsole = GetStdHandle(STD_ERROR_HANDLE))                           //возвращает 1 если дескиптор получен без ошибок, иначе 0
+    if (hWndConsole = GetStdHandle(STD_ERROR_HANDLE))                               //возвращает 1 если дескиптор получен без ошибок, иначе 0
     {
         CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
         if (GetConsoleScreenBufferInfo(hWndConsole, &consoleInfo))
@@ -29,7 +33,8 @@ int findWidthOfWindow()                                                         
     return Width;
 }
 
-int findHeightOfWindow()                                                        //Найдём высоту окна (сколько символов помещается по вертикали)
+//Находит высоту окна (в количестве символов)
+int findHeightOfWindow()
 {
     HWND hWndConsole;
     int Height = 0;
@@ -44,6 +49,9 @@ int findHeightOfWindow()                                                        
     return Height;
 }
 
+//Печатает строку char*
+//Начиная с указанных координат
+//attr - цвет текста и фона
 void printStrAtConsolePos(HANDLE hwd, int x, int y, char *str, WORD attr)
 {
     COORD pos;
@@ -56,15 +64,16 @@ void printStrAtConsolePos(HANDLE hwd, int x, int y, char *str, WORD attr)
 }
 
 //createField рисует игровое поле по середине консоли и делает клетки поля квадратными, длины сторон зависят от размеров окна
-void createField(HWND StdHandle, int size_x, int size_y)
+void createField(HWND StdHandle, int size_x, int size_y, int Color)
 {
     int Divisor_x = size_x / 3, Divisor_y = (size_y - 1) / 3, CommonDivisor = min(Divisor_x, Divisor_y) + Divisor_x / 3;
     char *FillLine = (char *) calloc(Divisor_x * 3 + 1, 1);
     for (int i = 0; i < CommonDivisor * 3; i++) FillLine[i] = '_';
-    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y, FillLine, BLACK_ON_WHITE);
-    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y, FillLine, BLACK_ON_WHITE);
-    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y * 2, FillLine, BLACK_ON_WHITE);
-    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y * 3, FillLine, BLACK_ON_WHITE);
+    FillLine[0] = ' ';
+    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y, FillLine, Color);
+    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y, FillLine, Color);
+    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y * 2, FillLine, Color);
+    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y + Divisor_y * 3, FillLine, Color);
     for (int cnt = 0; cnt <= 3; cnt++)
     {
         int current_x = LEFT_UPPER_ANGLE_OF_FIELD_X + cnt * CommonDivisor;
@@ -72,11 +81,10 @@ void createField(HWND StdHandle, int size_x, int size_y)
         {
             if (i != 0)
             {
-                printStrAtConsolePos(StdHandle, current_x, LEFT_UPPER_ANGLE_OF_FIELD_Y + i, "|", BLACK_ON_WHITE);
+                printStrAtConsolePos(StdHandle, current_x, LEFT_UPPER_ANGLE_OF_FIELD_Y + i, "|", Color);
             }
         }
     }
-    printStrAtConsolePos(StdHandle, LEFT_UPPER_ANGLE_OF_FIELD_X, LEFT_UPPER_ANGLE_OF_FIELD_Y, " ", FULL_WHITE);
 }
 
 //Очищает консоль, закрашивая её полностью в фоновый цвет
@@ -149,7 +157,9 @@ void drawCircle(HWND StdHandle, int size_x, int size_y, int x, int y)
     printStrAtConsolePos(StdHandle, current_x, current_y + 2, " \\____/ ", BLACK_ON_WHITE);
 }
 
-//Рисуем крестик или нолик в зависимости от того, чья сейчас очередь
+//Рисует крестик или нолик в зависимости от того, чья сейчас очередь
+//0 - ход первого игрока (крестики)
+//1 - ход второго игрока (нолики)
 int newMotion(int Turn, HWND WindowStdInputHandle, HWND ConsoleHandle, int size_x, int size_y, int x, int y)
 {
     if (Turn) drawCircle(WindowStdInputHandle, size_x, size_y, x, y);
@@ -158,6 +168,8 @@ int newMotion(int Turn, HWND WindowStdInputHandle, HWND ConsoleHandle, int size_
     return Turn % 2;
 }
 
+//Возвращает заполненную стрктуру COORD
+//С координатами текстового курсора
 COORD getTextCursorCoords()
 {
     CONSOLE_SCREEN_BUFFER_INFO info_x;
@@ -165,81 +177,69 @@ COORD getTextCursorCoords()
     return info_x.dwCursorPosition;
 }
 
+//Главная функция
+//Запускает саму игру
+//Рисует поле, фон и считывает координаты нажатия ЛКМ
+//В зависимости от порядка нажатия рисует в нужной клетке
+//Либо крестик, либо нолик
 void startGame(HWND WindowStdInputHandle, HWND ConsoleHandle)
 {
     clearWindow(WindowStdInputHandle);
-    createField(WindowStdInputHandle, findWidthOfWindow(), findHeightOfWindow());
+    createField(WindowStdInputHandle, findWidthOfWindow(), findHeightOfWindow(), BLACK_ON_WHITE);
     LPPOINT CursorCoordinates;
     CursorCoordinates = malloc(sizeof(*CursorCoordinates));
     int CellWidth = min(findWidthOfWindow() / 3, findHeightOfWindow() / 3) + findWidthOfWindow() / 9, CellHeight = findHeightOfWindow() / 3, Turn = 0;
-    while (GetKeyState(VK_ESCAPE) >= 0)
+    do
     {
         if (GetKeyState(VK_LBUTTON) < 0)
         {
             GetCursorPos(CursorCoordinates);
             ScreenToClient(ConsoleHandle, CursorCoordinates);
-            if (CursorCoordinates[0].x < LEFT_UPPER_ANGLE_OF_FIELD_X || CursorCoordinates[0].y < LEFT_UPPER_ANGLE_OF_FIELD_Y ||
-                CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + 3 * CellWidth ||
-                CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + 3 * CellHeight)
+            if (CursorCoordinates[0].x < LEFT_1X || CursorCoordinates[0].y < TOP_1Y || CursorCoordinates[0].x > RIGHT_3X || CursorCoordinates[0].y > BOTTOM_3Y)
             {
                 continue;
             }
             else
             {
-                if (CursorCoordinates[0].x >= LEFT_UPPER_ANGLE_OF_FIELD_X && CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                    CursorCoordinates[0].y >= LEFT_UPPER_ANGLE_OF_FIELD_Y && CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight)
+                if (CursorCoordinates[0].x >= LEFT_1X && CursorCoordinates[0].x <= RIGHT_1X && CursorCoordinates[0].y >= TOP_1Y && CursorCoordinates[0].y <= BOTTOM_1Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 1, 1);
                 }
-                else if (CursorCoordinates[0].x >= LEFT_UPPER_ANGLE_OF_FIELD_X && CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                         CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_1X && CursorCoordinates[0].x <= RIGHT_1X && CursorCoordinates[0].y >= TOP_2Y &&
+                         CursorCoordinates[0].y <= BOTTOM_2Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 1, 2);
                 }
-                else if (CursorCoordinates[0].x >= LEFT_UPPER_ANGLE_OF_FIELD_X && CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                         CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 3 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_1X && CursorCoordinates[0].x <= RIGHT_1X && CursorCoordinates[0].y >= TOP_3Y &&
+                         CursorCoordinates[0].y <= BOTTOM_3Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 1, 3);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].y >= LEFT_UPPER_ANGLE_OF_FIELD_Y && CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_2X && CursorCoordinates[0].x <= RIGHT_2X && CursorCoordinates[0].y >= TOP_1Y && CursorCoordinates[0].y <= BOTTOM_1Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 2, 1);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_2X && CursorCoordinates[0].x <= RIGHT_2X && CursorCoordinates[0].y >= TOP_2Y &&
+                         CursorCoordinates[0].y <= BOTTOM_2Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 2, 2);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 3 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_2X && CursorCoordinates[0].x <= RIGHT_2X && CursorCoordinates[0].y >= TOP_3Y &&
+                         CursorCoordinates[0].y <= BOTTOM_3Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 2, 3);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 3 * CellWidth &&
-                         CursorCoordinates[0].y >= LEFT_UPPER_ANGLE_OF_FIELD_Y && CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_3X && CursorCoordinates[0].x <= RIGHT_3X && CursorCoordinates[0].y >= TOP_1Y && CursorCoordinates[0].y <= BOTTOM_1Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 3, 1);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 3 * CellWidth &&
-                         CursorCoordinates[0].y >= LEFT_UPPER_ANGLE_OF_FIELD_Y + CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_3X && CursorCoordinates[0].x <= RIGHT_3X && CursorCoordinates[0].y >= TOP_2Y &&
+                         CursorCoordinates[0].y <= BOTTOM_2Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 3, 2);
                 }
-                else if (CursorCoordinates[0].x > LEFT_UPPER_ANGLE_OF_FIELD_X + 2 * CellWidth &&
-                         CursorCoordinates[0].x <= LEFT_UPPER_ANGLE_OF_FIELD_X + 3 * CellWidth &&
-                         CursorCoordinates[0].y > LEFT_UPPER_ANGLE_OF_FIELD_Y + 2 * CellHeight &&
-                         CursorCoordinates[0].y <= LEFT_UPPER_ANGLE_OF_FIELD_Y + 3 * CellHeight)
+                else if (CursorCoordinates[0].x >= LEFT_3X && CursorCoordinates[0].x <= RIGHT_3X && CursorCoordinates[0].y >= TOP_3Y &&
+                         CursorCoordinates[0].y <= BOTTOM_3Y)
                 {
                     Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), 3, 3);
                 }
@@ -247,9 +247,11 @@ void startGame(HWND WindowStdInputHandle, HWND ConsoleHandle)
             }
         }
         Sleep(100);
-    }
+    } while (GetKeyState(VK_ESCAPE) >= 0);
 }
 
+//Тестируем корректность считывания координат
+//Курсора мыши внутри окна консоли
 void testMouseClick(HWND WindowStdInputHandle)
 {
     LPPOINT CursorCoordinates;
@@ -266,6 +268,7 @@ void testMouseClick(HWND WindowStdInputHandle)
     } while (GetKeyState(VK_ESCAPE) >= 0);
 }
 
+//Полностью возвращает консоль в исходное состояние
 void resetConsole(HWND StdHandle)
 {
     int WidthOfWindow = findWidthOfWindow(), HeightOfWindow = findHeightOfWindow();
@@ -280,16 +283,18 @@ void resetConsole(HWND StdHandle)
     system("cls");
 }
 
+//Последовательно рисует в каждой клетке каждой строки
+//Поочерёдно либо крестик, либо нолик (для проверки)
 void checkAllFigures()
 {
-    HWND WindowStdInputHandle = makeWindow();
+    HWND WindowStdInputHandle = makeWindow(120, 30);
     HWND ConsoleHandle = GetConsoleWindow();
     clearWindow(WindowStdInputHandle);
-    createField(WindowStdInputHandle, findWidthOfWindow(), findHeightOfWindow());
+    createField(WindowStdInputHandle, findWidthOfWindow(), findHeightOfWindow(), BLACK_ON_WHITE);
     int Turn = 0;
-    for (int y=1; y<=3; y++)
+    for (int y = 1; y <= 3; y++)
     {
-        for (int x=1; x<=3; x++)
+        for (int x = 1; x <= 3; x++)
         {
             Turn = newMotion(Turn, WindowStdInputHandle, ConsoleHandle, findWidthOfWindow(), findHeightOfWindow(), x, y);
             Sleep(1000);
