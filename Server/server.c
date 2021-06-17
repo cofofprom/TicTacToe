46ebee0ce1f919a1f74b6b7e6cd6b61e218f92dc
Ailurus-code <76646255+Ailurus-code@users.noreply.github.com>
2021-06-17 21:49:00 +0300

    FUCK

--- Server/server.c
+++ Server/server.c
@@ -41,86 +41,169 @@ void* processRequest(void* arg)
         pthread_mutex_lock(&mutex);
         USR[USRSZ++] = *initUser(login, client); // юзверь авторизован
         curr = USRSZ - 1;
-        pthread_mutex_unlock(&mutex);
         printf("login is %s idx is %d sock is %d while inner is %d\n", login, curr, client, USR[curr].usersock);
+        pthread_mutex_unlock(&mutex);
         //send(USR[curr].usersock, login, strlen(login), 0);
         PACKET* ok = initPacketFromParams(ServicePacket, ServiceSuccess, 0, 0);
         char* okRaw = encodePacket(ok);
         pthread_mutex_lock(&mutex);
-        send(USR[curr].usersock, okRaw, strlen(okRaw), 0);
+        if (send(USR[curr].usersock, okRaw, strlen(okRaw), 0) == SOCKET_ERROR)
+        {
+            pthread_mutex_unlock(&mutex);
+            printf("Error in sending smth to usersock\n%s\n", okRaw);
+            return 0;
+        }
         pthread_mutex_unlock(&mutex);
         printf("USR[%d] is sent\n", curr);
     }
     int exitFlag = 0;
     //char boardSize;
     int opponent;
-    while(!exitFlag) {
+    while(!exitFlag)
+    {
         PACKET *inputData;
-        if(recvPacket(&USR[curr].usersock, &inputData) == SOCKET_ERROR) {
+        if (recvPacket(&USR[curr].usersock, &inputData) == SOCKET_ERROR)
+        {
+            pthread_mutex_lock(&mutex);
             USR[curr].online = 0;
             printf("USR[%d] known as '%s' became offline\n", curr, USR[curr].nickname);
+            pthread_mutex_unlock(&mutex);
             return 0;
         }
+        pthread_mutex_lock(&mutex);
         printf("thread of %s\n", USR[curr].nickname);
-        if(inputData->packetSubtype == ServiceUserAction && inputData->packetCode == GameRequestAction) {
+        pthread_mutex_unlock(&mutex);
+        if (inputData->packetSubtype == ServiceUserAction && inputData->packetCode == GameRequestAction)
+        {
             //boardSize = inputData->packetData[0];
+            pthread_mutex_lock(&mutex);
             int idx = findNickname(USR, USRSZ, (inputData->packetData + 2));
             SOCKET opponent = USR[idx].usersock;
-            char* inviteData = (char*)calloc(strlen(USR[curr].nickname) + 2, sizeof(char));
+            char *inviteData = (char *) calloc(strlen(USR[curr].nickname) + 2, sizeof(char));
+            pthread_mutex_unlock(&mutex);
             inviteData[0] = 3;
-            inviteData[1] = (char)strlen(USR[curr].nickname);
-            for(int i = 0; i < inviteData[1]; i++) {
+            pthread_mutex_lock(&mutex);
+            inviteData[1] = (char) strlen(USR[curr].nickname);
+            pthread_mutex_unlock(&mutex);
+            for (int i = 0; i < inviteData[1]; i++)
+            {
+                pthread_mutex_lock(&mutex);
                 inviteData[i + 2] = USR[curr].nickname[i];
+                pthread_mutex_unlock(&mutex);
             }
-            PACKET* otherInvite = initPacketFromParams(ServicePacket, ServiceUserAction, GameRequestAction, inviteData);
-            char* rawInvite = encodePacket(otherInvite);
+            PACKET *otherInvite = initPacketFromParams(ServicePacket, ServiceUserAction, GameRequestAction, inviteData);
+            char *rawInvite = encodePacket(otherInvite);
+            pthread_mutex_lock(&mutex);
             printf("invite %s -> %s\n", USR[curr].nickname, USR[idx].nickname);
-            send(opponent, rawInvite, strlen(rawInvite), 0);
+            pthread_mutex_unlock(&mutex);
+            if (send(opponent, rawInvite, strlen(rawInvite), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending smth to opponent_1\n%s\n", rawInvite);
+                return 0;
+            }
+            pthread_mutex_lock(&mutex);
             USR[curr].role = CrossCell;
             USR[curr].opponentID = idx;
             USR[curr].game = initNewBoard(3);
+            pthread_mutex_unlock(&mutex);
         }
-        else if(inputData->packetSubtype == ServiceUserAction && inputData->packetCode == GameAcceptAction) {
+        else if (inputData->packetSubtype == ServiceUserAction && inputData->packetCode == GameAcceptAction)
+        {
+            pthread_mutex_lock(&mutex);
             USR[curr].game = initNewBoard(3);
             USR[curr].role = ZeroCell;
-            for(int i = 0; i < USRSZ; i++) {
-                if(USR[i].opponentID == curr) {
+            pthread_mutex_unlock(&mutex);
+            for (int i = 0; i < USRSZ; i++)
+            {
+                if (USR[i].opponentID == curr)
+                {
+                    pthread_mutex_lock(&mutex);
                     USR[curr].opponentID = i;
+                    pthread_mutex_unlock(&mutex);
                     break;
                 }
             }
+            pthread_mutex_lock(&mutex);
             printf("accept %s -> %s\n", USR[curr].nickname, USR[USR[curr].opponentID].nickname);
-            PACKET* ok = initPacketFromParams(ServicePacket, ServiceSuccess, 0, 0);
-            char* okRaw = encodePacket(ok);
-            send(USR[USR[curr].opponentID].usersock, okRaw, strlen(okRaw), 0);
+            pthread_mutex_unlock(&mutex);
+            PACKET *ok = initPacketFromParams(ServicePacket, ServiceSuccess, 0, 0);
+            char *okRaw = encodePacket(ok);
+            pthread_mutex_lock(&mutex);
+            if (send(USR[USR[curr].opponentID].usersock, okRaw, strlen(okRaw), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending smth to opponent_2\n%s\n", okRaw);
+                return 0;
+            }
             printf("%s playing with %s\n", USR[curr].nickname, USR[USR[curr].opponentID].nickname);
             printf("curr is %d, opponent is %d", USR[curr].role, USR[USR[curr].opponentID].role);
-            char* boardEncoding = encodeBoard(USR[curr].game);
-            PACKET* requestMove = initPacketFromParams(DataRequestPacket, RequestPlayerMove, 0, boardEncoding);
-            char* requestMoveRaw = encodePacket(requestMove);
-            send(USR[curr].usersock, requestMoveRaw, strlen(requestMoveRaw), 0);
+            char *boardEncoding = encodeBoard(USR[curr].game);
+            pthread_mutex_unlock(&mutex);
+            PACKET *requestMove = initPacketFromParams(DataRequestPacket, RequestPlayerMove, 0, boardEncoding);
+            char *requestMoveRaw = encodePacket(requestMove);
+            pthread_mutex_lock(&mutex);
+            if (send(USR[curr].usersock, requestMoveRaw, strlen(requestMoveRaw), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending requestMove\n%s\n", requestMoveRaw);
+                return 0;
+            }
             printf("\nrequest move -> %s\n", USR[curr].nickname);
+            pthread_mutex_unlock(&mutex);
         }
-        else if(inputData->packetSubtype == SendPlayerMove) {
+        else if (inputData->packetSubtype == ServiceUserAction && inputData->packetCode == GameDeclineAction)
+        {
+            printf("The opponent declined the offer\n");
+            //Предложение отклонено...
+            return 0;
+        }
+        else if (inputData->packetSubtype == SendPlayerMove)
+        {
             char x = inputData->packetData[0] - 1;
             char y = inputData->packetData[1] - 1;
-            char* resend = encodePacket(inputData);
+            char *resend = encodePacket(inputData);
+            pthread_mutex_lock(&mutex);
             printf("%s moving on %d, %d. Sending this to %s\n", USR[curr].nickname, x, y, USR[USR[curr].opponentID].nickname);
-            send(USR[USR[curr].opponentID].usersock, resend, strlen(resend), 0);
-            PACKET* ok = initPacketFromParams(ServicePacket, ServiceSuccess, 0, 0);
-            char* okRaw = encodePacket(ok);
-            send(USR[curr].usersock, okRaw, strlen(okRaw), 0);
+            if (send(USR[USR[curr].opponentID].usersock, resend, strlen(resend), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending smth to opponent_3\n%s\n", resend);
+                return 0;
+            }
+            pthread_mutex_unlock(&mutex);
+            PACKET *ok = initPacketFromParams(ServicePacket, ServiceSuccess, 0, 0);
+            char *okRaw = encodePacket(ok);
+            pthread_mutex_lock(&mutex);
+            if (send(USR[curr].usersock, okRaw, strlen(okRaw), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending smth to opponent_4\n%s\n", okRaw);
+                return 0;
+            }
             makeMove(USR[curr].game, x, y, USR[curr].role);
             makeMove(USR[USR[curr].opponentID].game, x, y, USR[curr].role);
-            char* boardEncoding = encodeBoard(USR[curr].game);
-            PACKET* requestMove = initPacketFromParams(DataRequestPacket, RequestPlayerMove, 0, boardEncoding);
-            char* requestMoveRaw = encodePacket(requestMove);
-            send(USR[USR[curr].opponentID].usersock, requestMoveRaw, strlen(requestMoveRaw), 0);
+            char *boardEncoding = encodeBoard(USR[curr].game);
+            pthread_mutex_unlock(&mutex);
+            PACKET *requestMove = initPacketFromParams(DataRequestPacket, RequestPlayerMove, 0, boardEncoding);
+            char *requestMoveRaw = encodePacket(requestMove);
+            pthread_mutex_lock(&mutex);
+            if (send(USR[USR[curr].opponentID].usersock, requestMoveRaw, strlen(requestMoveRaw), 0) == SOCKET_ERROR)
+            {
+                pthread_mutex_unlock(&mutex);
+                printf("Error in sending smth to opponent\n%s\n", requestMoveRaw);
+                return 0;
+            }
+            pthread_mutex_unlock(&mutex);
         }
+        pthread_mutex_lock(&mutex);
         winner = checkBoardWinConditions(USR[curr].game);
-        if(USR[curr].game && winner != GAMEBOARD_NO_WIN) exitFlag = 1;
+        if (USR[curr].game && winner != GAMEBOARD_NO_WIN) exitFlag = 1;
+        pthread_mutex_unlock(&mutex);
     }
+    pthread_mutex_lock(&mutex);
     if(USR[curr].role == winner) printf("%s has won!!\n", USR[curr].nickname);
+    pthread_mutex_unlock(&mutex);
     printf("someone has won!\n");
 }
 
